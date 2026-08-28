40a07b3f5c7262818d90c1e9418a29ef75c804ad
Aswin1255 <aswinvinod1255@gmail.com>
2026-08-28 23:12:24 +0530

    Update portfolio

--- script.js
+++ script.js
@@ -86,10 +86,10 @@ const roles = [
   'Threat Hunter',
   //'Red Team Operator',
   'Incident Responder',
-  'Network Engineer',
-  'Network Technician',
-  'Network Admin',
-  'IT Support'
+  //'Network Engineer',
+  //'Network Technician',
+  //'Network Admin',
+  //'IT Support'
 ];
 let roleIdx = 0, charIdx = 0, deleting = false;
 const typedEl = document.getElementById('typedText');
